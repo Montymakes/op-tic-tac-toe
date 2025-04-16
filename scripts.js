28dc14e64f8b5889b4ad30c58c943595abf630db
Monty <montydmontilla@gmail.com>
2025-04-15 22:19:10 -0400

    Add ability to click spaces on the board to play.
    Comment out unused game logic functions.

--- scripts.js
+++ scripts.js
@@ -1,7 +1,12 @@
 const gameDisplay = document.querySelector("#game-board");
 const newGameModal = document.querySelector("dialog");
 const startGameButton = document.querySelector("#activate-game");
-
+/**
+ * 
+ * @param {string} playerToken 'X' or 'O' determined by player order.
+ * @param {string} playerName  Provided by user input
+ * @returns 
+ */
 function Player(playerToken, playerName) {
     const token = playerToken; 
     const name = playerName;
@@ -16,151 +21,150 @@ const Gameboard = (function () {
     const rows = 3;
     const columns = 3;
     const board = [];
-
+    const emptycells = [];
     const getBoard = () => board;
+    const getEmptyCells = () => emptycells;
 
-    for (let i = 0; i < rows; i++) {
-        board[i] = [];
-        for (let j = 0; j < columns; j++) {
-            board[i].push(' ');
-        }
-    }
+    function Cell(row, column) {
+        const location = `cell-${row}-${column}`;
+        let token = ' ';
+        let isEmpty = true;
+        
+        const getToken = () => token;
 
-    const dropToken = (row, column, player) => {
-        if (board[row][column] !== ' ') {
-            return false;
-        }
-        else {
-            board[row][column] = player.getToken();
-            return true;
-        }
+        //Called when an empty cell is clicked
+        const updateToken = (playerToken) => {
+            token = playerToken;
+            isEmpty = false;
+        };
+        
+        return {location, isEmpty, getToken, updateToken};
     };
-    
-    const clearBoard = () => {
-        for (let i = 0; i < rows; i++) {
-            board[i] = [];
-            for (let j = 0; j < columns; j++) {
-                board[i][j] = ' ';
+
+    const initializeBoard = () => {
+        for (let row = 0; row < rows; row++) {
+            board[row] = [];
+            for (let column = 0; column < columns; column++) {
+                const cell = Cell(row,column)
+                board[row][column] = cell;
+                emptycells.push(cell.location);
             }
         }
     };
-
-    const getWinner = () => {
-        const player1Wins = 'XXX';
-        const player2Wins = 'OOO';
-        const horizontal1 = board[0].join('');
-        const horizontal2 = board[1].join('');
-        const horizontal3 = board[2].join('');
-        const vertical1 = board[0][0] + board[1][0] + board[2][0];
-        const vertical2 = board[0][1] + board[1][1] + board[2][1];
-        const vertical3 = board[0][2] + board[1][2] + board[2][2];
-        const diagonal1 = board[0][0] + board[1][1] + board[2][2];
-        const diagonal2 = board[2][0] + board[1][1] + board[0][2];
-
-        if( player1Wins === horizontal1 
-            || player1Wins === horizontal2 
-            || player1Wins === horizontal3 
-            || player1Wins === vertical1
-            || player1Wins === vertical2
-            || player1Wins === vertical3
-            || player1Wins === diagonal1
-            || player1Wins === diagonal2
-        ) return 'X';
-
-        if( player2Wins === horizontal1 
-            || player2Wins === horizontal2 
-            || player2Wins === horizontal3 
-            || player2Wins === vertical1
-            || player2Wins === vertical2
-            || player2Wins === vertical3
-            || player2Wins === diagonal1
-            || player2Wins === diagonal2
-        ) return 'O';
-
-        return false;
-    };
-
-    return {dropToken, clearBoard, getBoard, getWinner};
+    
+    return {initializeBoard, getBoard, Cell, getEmptyCells};
 })();
 
 const GameController = (function (player1Name,player2Name,rounds) {
     const player1 = Player('X', player1Name);
     const player2 = Player('O', player2Name);
     const totalRounds = rounds;
+    const board = Gameboard.getBoard();
+    const emptycells = Gameboard.getEmptyCells();
     let turnPlayer = player1;
     let round = 1;
     let turn = 1;
     let gameOver = false;
 
-    const switchPlayer = () => {
-        turnPlayer = turnPlayer === player1 ? player2 : player1;
-    };
-    
-    const playRound = (row, column) => {
-        if (gameOver) {
-            return;
+    const dropToken = (row, column, player) => {
+        const cell = board[row][column];
+        if (cell.isEmpty) {
+            cell.updateToken(player.getToken());
+            emptycells.splice(emptycells.indexOf(cell.location),1);
+            GameDOMController.displayBoard();
+            return true        
         }
-
-        if(round > 9) {
-            gameOver = true;
-            return;
-        }
-
-        turnPlayer.promptTurn();
-
-        let validTurn = Gameboard.dropToken(row, column, turnPlayer);
-        if (validTurn) {
-            Gameboard.displayBoard();
-
-            if (Gameboard.getWinner()) {
-                gameOver = true;
-                turnPlayer.winningMessage();
-                return;
-            }
-            switchPlayer();
-            turn++;
+        else {
+            console.warn('IMPOSSIBLE MOVE!');
+            return false;
         }
     };
+
+    // const getWinner = () => {
+    //     const player1Wins = 'XXX';
+    //     const player2Wins = 'OOO';
+    //     const horizontal1 = board[0].join('');
+    //     const horizontal2 = board[1].join('');
+    //     const horizontal3 = board[2].join('');
+    //     const vertical1 = board[0][0] + board[1][0] + board[2][0];
+    //     const vertical2 = board[0][1] + board[1][1] + board[2][1];
+    //     const vertical3 = board[0][2] + board[1][2] + board[2][2];
+    //     const diagonal1 = board[0][0] + board[1][1] + board[2][2];
+    //     const diagonal2 = board[2][0] + board[1][1] + board[0][2];
+
+    //     if( player1Wins === horizontal1 
+    //         || player1Wins === horizontal2 
+    //         || player1Wins === horizontal3 
+    //         || player1Wins === vertical1
+    //         || player1Wins === vertical2
+    //         || player1Wins === vertical3
+    //         || player1Wins === diagonal1
+    //         || player1Wins === diagonal2
+    //     ) return 'X';
+
+    //     if( player2Wins === horizontal1 
+    //         || player2Wins === horizontal2 
+    //         || player2Wins === horizontal3 
+    //         || player2Wins === vertical1
+    //         || player2Wins === vertical2
+    //         || player2Wins === vertical3
+    //         || player2Wins === diagonal1
+    //         || player2Wins === diagonal2
+    //     ) return 'O';
+
+    //     return false;
+    // };
+
+    // const switchPlayer = () => {
+    //     turnPlayer = turnPlayer === player1 ? player2 : player1;
+    // };
 
     const restartGame = () => {
-        Gameboard.clearBoard();
+        Gameboard.initializeBoard();
         turn = 1;
         round = 1;
         gameOver = false;
     };
 
-    return {playRound, restartGame};
+    return {restartGame, turnPlayer, dropToken};
 })();
 
 const GameDOMController = (function () {
     const board = Gameboard.getBoard();
+    const emptycells = Gameboard.getEmptyCells();
 
     const displayBoard = () => {
         let currentBoard = "";
 
         for (let i = 0; i < 3; i++) {
             for (let j = 0; j < 3; j++) {
-                currentBoard +=`<div class="game-cell" id="cell-${i}-${j}"><p class="player-token">${board[i][j]}</p></div>`;
+                const cell = board[i][j];
+                currentBoard +=`<div class="game-cell ${cell.isEmpty ? "clickable-cell" : ""}" id="cell-${i}-${j}" data-row="${i}" data-column="${j}"><p class="player-token">${cell.getToken()}</p></div>`;
             }
         }
 
         gameDisplay.innerHTML = currentBoard;
-    };
 
-    const displayNewGameSettings = () => {newGameModal.showModal()};
+        emptycells.forEach(cell => {
+            const cellDOM = document.querySelector(`#${cell}`);
+            cellDOM.addEventListener('click', () => GameController.dropToken(cellDOM.dataset.row,cellDOM.dataset.column,GameController.turnPlayer));
+        });
+    };
 
     startGameButton.addEventListener("click", (e) => {
         e.preventDefault();
+        Gameboard.initializeBoard();
         displayBoard();
         gameDisplay.classList.remove("inactive-game");
         gameDisplay.classList.add("active-game");
         newGameModal.close();
+        gameDisplay.removeEventListener('click', displayNewGameSettings);
     });
+
+    const displayNewGameSettings = () => newGameModal.showModal();
 
     const displayNewGameScreen = () => {
         gameDisplay.addEventListener("click", displayNewGameSettings);
-
-        
     };
 
     displayNewGameScreen();
