794f7651a674a5babb11c26ab16934187ea3cc34
Monty <montydmontilla@gmail.com>
2025-04-17 17:47:56 -0400

    Add output to DOM to prompt players on their turn.

--- scripts.js
+++ scripts.js
@@ -1,6 +1,10 @@
 const gameDisplay = document.querySelector("#game-board");
 const newGameModal = document.querySelector("dialog");
 const startGameButton = document.querySelector("#activate-game");
+const player1NameInput = document.querySelector("#player-1-name");
+const player2NameInput = document.querySelector("#player-2-name");
+const roundInput = document.querySelector('input[name="rounds"]');
+const output = document.querySelector('#output');
 /**
  * 
  * @param {string} playerToken 'X' or 'O' determined by player order.
@@ -12,18 +16,18 @@ function Player(playerToken, playerName) {
     const name = playerName;
 
     const getToken = () => token;
+    const getName = () => name;
     const promptTurn = () => `${name} will mark a space`;
     const winningMessage = () => `${name} won the game!`;
-    return {getToken, promptTurn, winningMessage};
+    return {getToken, getName, promptTurn, winningMessage};
 };
 
 const Gameboard = (function () {
     const rows = 3;
     const columns = 3;
     const board = [];
-    const emptycells = [];
-    const getBoard = () => board;
-    const getEmptyCells = () => emptycells;
+    const emptyCells = [];
+    const getEmptyCells = () => emptyCells;
 
     function Cell(row, column) {
         const location = `cell-${row}-${column}`;
@@ -47,26 +51,31 @@ const Gameboard = (function () {
             for (let column = 0; column < columns; column++) {
                 const cell = Cell(row,column)
                 board[row][column] = cell;
-                emptycells.push(cell.location);
+                emptyCells.push(cell.location);
             }
         }
     };
-    
-    return {initializeBoard, getBoard, Cell, getEmptyCells};
-})();
 
-const GameController = (function (player1Name,player2Name,rounds) {
-    const player1 = Player('X', player1Name);
-    const player2 = Player('O', player2Name);
-    const totalRounds = rounds;
-    const board = Gameboard.getBoard();
-    const emptycells = Gameboard.getEmptyCells();
+    initializeBoard();
+    
+    return {Cell, getEmptyCells, board};
+});
+
+const GameController = (function (player1, player2, rounds) {
+    player1, player2, rounds
     let turnPlayer = player1;
+    const boardController = Gameboard();
+    const board = boardController.board;
+    const emptyCells = boardController.getEmptyCells();
     let round = 1;
     let turn = 1;
     let gameOver = false;
 
     const getTurnPlayerToken = () => turnPlayer.getToken();
+    const getTurnPlayerName = () => turnPlayer.getName();
+    const promptTurnPlayer = () => turnPlayer.promptTurn();
+    const displayWinner = () => turnPlayer.winningMessage();
+    const getBoard = () => board;
 
     const switchPlayer = () => {
         turnPlayer = turnPlayer === player1 ? player2 : player1;
@@ -76,9 +85,10 @@ const GameController = (function (player1Name,player2Name,rounds) {
         const cell = board[row][column];
         if (cell.isEmpty) {
             cell.updateToken(getTurnPlayerToken());
-            emptycells.splice(emptycells.indexOf(cell.location),1);
+            emptyCells.splice(emptyCells.indexOf(cell.location),1);
             GameDOMController.displayBoard();
             switchPlayer();
+            GameDOMController.promptTurnOutput();
             return true        
         }
         else {
@@ -87,84 +97,68 @@ const GameController = (function (player1Name,player2Name,rounds) {
         }
     };
 
-    // const getWinner = () => {
-    //     const player1Wins = 'XXX';
-    //     const player2Wins = 'OOO';
-    //     const horizontal1 = board[0].join('');
-    //     const horizontal2 = board[1].join('');
-    //     const horizontal3 = board[2].join('');
-    //     const vertical1 = board[0][0] + board[1][0] + board[2][0];
-    //     const vertical2 = board[0][1] + board[1][1] + board[2][1];
-    //     const vertical3 = board[0][2] + board[1][2] + board[2][2];
-    //     const diagonal1 = board[0][0] + board[1][1] + board[2][2];
-    //     const diagonal2 = board[2][0] + board[1][1] + board[0][2];
-
-    //     if( player1Wins === horizontal1 
-    //         || player1Wins === horizontal2 
-    //         || player1Wins === horizontal3 
-    //         || player1Wins === vertical1
-    //         || player1Wins === vertical2
-    //         || player1Wins === vertical3
-    //         || player1Wins === diagonal1
-    //         || player1Wins === diagonal2
-    //     ) return 'X';
-
-    //     if( player2Wins === horizontal1 
-    //         || player2Wins === horizontal2 
-    //         || player2Wins === horizontal3 
-    //         || player2Wins === vertical1
-    //         || player2Wins === vertical2
-    //         || player2Wins === vertical3
-    //         || player2Wins === diagonal1
-    //         || player2Wins === diagonal2
-    //     ) return 'O';
-
-    //     return false;
-    // };
-
-   
-
-    const restartGame = () => {
-        Gameboard.initializeBoard();
-        turn = 1;
-        round = 1;
-        gameOver = false;
-    };
-
-    return {restartGame, getTurnPlayerToken, dropToken};
-})();
+    return {getTurnPlayerToken, getTurnPlayerName, promptTurnPlayer, displayWinner, dropToken, getBoard, boardController};
+});
 
 const GameDOMController = (function () {
-    const board = Gameboard.getBoard();
-    const emptycells = Gameboard.getEmptyCells();
+    let game, board, boardController;
 
-    const displayBoard = () => {
-        let currentBoard = "";
-
+    const createBoardDisplay = () => {
+        let newBoard = "";
         for (let i = 0; i < 3; i++) {
             for (let j = 0; j < 3; j++) {
                 const cell = board[i][j];
-                currentBoard +=`<div class="game-cell" id="cell-${i}-${j}" data-row="${i}" data-column="${j}"><p class="player-token">${cell.getToken()}</p></div>`;
+                newBoard +=`<div class="game-cell" id="cell-${i}-${j}" data-row="${i}" data-column="${j}"><p class="player-token">${cell.getToken()}</p></div>`;
             }
         }
+        return newBoard;
+    };
 
-        gameDisplay.innerHTML = currentBoard;
-
-        emptycells.forEach(cell => {
+    const displayBoard = () => {
+        gameDisplay.innerHTML = createBoardDisplay();
+        const emptyCells = boardController.getEmptyCells();
+        emptyCells.forEach(cell => {
             const cellDOM = document.querySelector(`#${cell}`);
             cellDOM.classList.add('clickable-cell');
-            cellDOM.addEventListener('click', () => GameController.dropToken(cellDOM.dataset.row,cellDOM.dataset.column,GameController.getTurnPlayerToken()));
+            cellDOM.addEventListener('click', () => game.dropToken(cellDOM.dataset.row,cellDOM.dataset.column,game.getTurnPlayerToken()));
         });
     };
 
+    const validateInputs = () => {
+        if (player1NameInput.value.length < 1 | player2NameInput.value.length < 1) {
+            document.querySelector("#invalid-input").hidden = false;
+            console.warn('invalid input');
+            return false;
+        }
+        return true;
+    };
+
+    const promptTurnOutput = () => {
+        output.textContent = game.promptTurnPlayer();
+        output.hidden = false;
+    };
+
+    function startGame(event) {
+        event.preventDefault();
+
+        if (validateInputs()) {
+            document.querySelector("#invalid-input").hidden = true;
+            game = GameController(Player('X', player1NameInput.value), Player('O', player2NameInput.value), Number(roundInput.checked.value));
+            boardController = game.boardController;
+            board = boardController.board;
+
+            promptTurnOutput();
+            displayBoard();
+
+            gameDisplay.classList.remove("inactive-game");
+            gameDisplay.classList.add("active-game");
+            newGameModal.close();
+            gameDisplay.removeEventListener('click', displayNewGameSettings);
+        }
+    }
+
     startGameButton.addEventListener("click", (e) => {
-        e.preventDefault();
-        Gameboard.initializeBoard();
-        displayBoard();
-        gameDisplay.classList.remove("inactive-game");
-        gameDisplay.classList.add("active-game");
-        newGameModal.close();
-        gameDisplay.removeEventListener('click', displayNewGameSettings);
+       startGame(e);
     });
 
     const displayNewGameSettings = () => newGameModal.showModal();
@@ -175,6 +169,6 @@ const GameDOMController = (function () {
 
     displayNewGameScreen();
 
-    return {displayBoard};
+    return {displayBoard, promptTurnOutput};
 })();
 
